9cb7ca74997b6f326cc10b317ab10fb9172cb343
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-12-07 12:46:56 +0100

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :logger, level: :info
 
 # Configure your database
 
-config :codestoriesstats, CodestoriesstatsWeb.Repo,
+config :codestoriesstats, Codestoriesstats.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
