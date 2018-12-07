c46c1e9be45791ef873eb4ad259e6ac7ae7fbcce
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-12-07 13:23:06 +0100

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :codestoriesstats, CodestoriesstatsWeb.Endpoint,
-  http: [:inet6, port: {:system, :integer, "PORT", 4000}],
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "codestories-stats.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
