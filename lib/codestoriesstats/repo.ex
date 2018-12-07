f863625875a74cbff1dcc1daf5e61b19ace2742e
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-12-07 11:19:40 +0100

    add confex

--- lib/codestoriesstats/repo.ex
+++ lib/codestoriesstats/repo.ex
@@ -2,4 +2,9 @@ defmodule Codestoriesstats.Repo do
   use Ecto.Repo,
     otp_app: :codestoriesstats,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+    config = Confex.Resolver.resolve!(config)
+    {:ok, config}
+  end
 end
