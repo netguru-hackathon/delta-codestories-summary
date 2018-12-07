aff8bc442bcea48a716105d9d5b294819cb1f148
Marcin Szmigiel <marcin.szmigiel@netguru.co>
2018-12-07 11:02:41 +0100

    Update repo.ex

--- lib/codestoriesstats/repo.ex
+++ lib/codestoriesstats/repo.ex
@@ -2,9 +2,4 @@ defmodule Codestoriesstats.Repo do
   use Ecto.Repo,
     otp_app: :codestoriesstats,
     adapter: Ecto.Adapters.Postgres
-
-  def init(_, config) do
-    config = Confex.Resolver.resolve!(config)
-    {:ok, config}
-  end
 end
